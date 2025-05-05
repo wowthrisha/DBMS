47f8144bd224970b759a717a2f84b24150f369d5
eswarisankar5 <eswarisankar0@gmail.com>
2025-05-05 15:52:57 +0530

    update backend

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,12 +1,12 @@
 import express from "express";
+import { addToCart, getCartProducts, removeAllFromCart, updateQuantity } from "../controllers/cart.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
-//import { addToCart } from "../controllers/cart.controller.js";
-import { getAllProducts } from "../controllers/product.controller.js";
 const router = express.Router();
-router.get("/",protectRoute,getAllProducts);
-//router.post("/",protectRoute,addToCart);
-//router.delete("/",protectRoute,removeAllFromCart);
-//router.put("/:id",protectRoute,updateQuantity);
+
+router.get("/", protectRoute, getCartProducts);
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
 
 export default router;
